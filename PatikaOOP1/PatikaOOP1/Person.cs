99cdd6da99dd97e2e33c0f375fd661b36a4f1517
Ezgi Kececi Atalay <ezgikececii@gmail.com>
2024-08-05 20:59:16 +0300

    Add files via upload

---
+++ PatikaOOP1/PatikaOOP1/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatikaOOP1
+{
+    public class Person
+    {
+        //Person sınıfına ait field'lar
+        private string name;
+        private string surname;
+        private int birthDate;
+
+        //Properties
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public string Surname
+        {
+            get { return surname; }
+            set { surname = value; }
+        }
+
+        public int BirthDate
+        {
+            get { return birthDate; }
+
+            set
+            {
+                if (value > 0)
+                {
+                    birthDate = value;
+                }
+                else
+                    Console.WriteLine("Yanlış bir tarih girildi.");
+            }
+        }
+
+        //Kişi detaylarını konsola yazdırmak için metot oluşturma
+        public void TeacherInfo()
+        {
+            Console.WriteLine($"Öğretmenin adı : {Name}\nÖğretmenin soyadı : {surname}\nÖğretmenin doğum yılı: {birthDate}");
+
+        }
+
+        public void StudentInfo()
+        {
+            Console.WriteLine($"Öğrencinin adı : {name}\nÖğrencinin soyadı : {surname}\nÖğrencinin doğum yılı : {birthDate}");
+        }
+    }
+}
---
+++ PatikaOOP1/PatikaOOP1/Program.cs
@@ -0,0 +1,38 @@
+﻿using PatikaOOP1;
+
+public class Program
+{
+    public static void Main()
+    {
+        // Person sınıfından nesneler oluşturma
+        Person student1 = new Person();
+        student1.Name = "Mete";
+        student1.Surname = "Gazoz";
+        student1.BirthDate = 1999;
+
+        Person student2 = new Person();
+        student2.Name = "Buse";
+        student2.Surname = "Sürmeneli";
+        student2.BirthDate = 1998;
+
+        Person teacher1 = new Person();
+        teacher1.Name = "Göktuğ";
+        teacher1.Surname = "Ergin";
+        teacher1.BirthDate = 1984;
+
+        Person teacher2 = new Person();
+        teacher2.Name = "Cahit";
+        teacher2.Surname = "Sürme";
+        teacher2.BirthDate = 1985;
+
+        //Person sınıfındaki metodları ilgili kişiler için çağırma
+        student1.StudentInfo();
+        Console.WriteLine();
+        student2.StudentInfo();
+        Console.WriteLine();
+        teacher1.TeacherInfo();
+        Console.WriteLine();
+        teacher2.TeacherInfo();
+    }
+}
+
